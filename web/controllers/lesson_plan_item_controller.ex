cec9002a99623cb78b0978de1b640a707bbcd6a0
WANG QIANG <rovingbreeze@gmail.com>
2016-09-15 00:35:29 +0800

    Speed up lesson plan page by preload assessments and events

--- web/controllers/lesson_plan_item_controller.ex
+++ web/controllers/lesson_plan_item_controller.ex
@@ -6,7 +6,7 @@ defmodule Coursemology.LessonPlanItemController do
 
   def index(conn, _params) do
     course = conn.assigns[:course]
-    |> Coursemology.Repo.preload(lesson_plan_items: (from l in LessonPlanItem, order_by: l.start_at))
+    |> Coursemology.Repo.preload([lesson_plan_items: (from l in LessonPlanItem, order_by: l.start_at), lesson_plan_items: [assessment: [tab: :category]], lesson_plan_items: :event])
     |> Coursemology.Repo.preload(lesson_plan_milestones: (from m in LessonPlanMilestone, order_by: m.start_at))
 
     milestone_with_items = grouped_item_and_milestone(course.lesson_plan_items, course.lesson_plan_milestones)
--- web/views/lesson_plan_item_view.ex
+++ web/views/lesson_plan_item_view.ex
@@ -4,10 +4,12 @@ defmodule Coursemology.LessonPlanItemView do
   def render_item(item) do
     case item.actable_type do
       "Course::LessonPlan::Event" ->
-        event = Coursemology.LessonPlanEvent.for_item(item)
+        # event = Coursemology.LessonPlanEvent.for_item(item)
+        event = item.event
         render "_event.html", event: event, item: item
       "Course::Assessment" ->
-        assessment = Coursemology.Assessment.for_item(item)
+        # assessment = Coursemology.Assessment.for_item(item)
+        assessment = item.assessment
         render "_assessment.html", assessment: assessment, item: item
     end
   end
